1414509b6c69c150e4f7ea564d58623738ba511f
vqthinh <vqthinh110594@gmail.com>
2016-06-07 21:18:01 +0700

    Add Models

---
+++ QTMvc5Shop.Model/Models/Page.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using QTMvc5Shop.Model.Abstract;
+
+namespace QTMvc5Shop.Model.Models
+{
+    [Table("Pages")]
+    public class Page : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(256)]
+        [Required]
+        public string Alias { set; get; }
+
+        public string Content { set; get; }
+    }
+}
