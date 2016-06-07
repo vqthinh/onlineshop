ebde517554dba1c326cda2564135a489bc04da88
vqthinh <vqthinh110594@gmail.com>
2016-06-07 22:14:30 +0700

    Add Data

---
+++ QTMvc5Shop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace QTMvc5Shop.Data.Infrastructure
+{
+    public interface IDbFactory : IDisposable
+    {
+        QTMvc5ShopDbContext Init();
+    }
+}
---
+++ QTMvc5Shop.Data/QTMvc5ShopDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QTMvc5Shop.Model.Models;
+
+namespace QTMvc5Shop.Data
+{
+    public class QTMvc5ShopDbContext : DbContext
+    {
+        public QTMvc5ShopDbContext() : base("QTMvc5ShopDbContext")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Footer> Footers { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<Slide> Slides { get; set; }
+        public DbSet<SupportOnline> SupportOnlines { get; set; }
+        public DbSet<SystemConfig> SystemConfigs { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<VisitorStatistic> VisitorStatistics { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
