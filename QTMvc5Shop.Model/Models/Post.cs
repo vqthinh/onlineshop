f5e493dd0e662dad64508933eb751d9f219c7364
vqthinh <vqthinh110594@gmail.com>
2016-06-07 23:49:58 +0700

    Add PostService

--- QTMvc5Shop.Model/Models/Post.cs
+++ QTMvc5Shop.Model/Models/Post.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using QTMvc5Shop.Model.Abstract;
 
@@ -37,5 +38,7 @@ namespace QTMvc5Shop.Model.Models
 
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
+
+        public virtual IEnumerable<PostTag> PostTags { set; get; } 
     }
 }
---
+++ QTMvc5Shop.Service/PostService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QTMvc5Shop.Data.Infrastructure;
+using QTMvc5Shop.Data.Repositories;
+using QTMvc5Shop.Model.Models;
+
+namespace QTMvc5Shop.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAllPaging(int page, int pageSize,out int totalRow);
+        Post GetById(int id);
+        IEnumerable<Post> GetAllByTagPaging(string tag,int page, int pageSize, out int totalRow);
+        void SaveChange(); 
+
+    }
+    public class PostService : IPostService
+    {
+        IPostRepository _postRepository;
+        IUnitOfWork _unitOfWork;
+        public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork)
+        {
+            _postRepository = postRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public void Add(Post post)
+        {
+            _postRepository.Add(post);
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+
+        public void Delete(int id)
+        {
+           _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _postRepository.GetAll(new[] {"PostCategory"});
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(string tag,int page, int pageSize, out int totalRow)
+        {
+            //TODO: Select all post by tag
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public void SaveChange()
+        {
+            _unitOfWork.Commit(); 
+        }
+    }
+}
